3824294084450849e95b11891fae4ffd38b74c65
Anton Grin <kanes@inbox.ru>
2023-05-15 02:49:12 +0300

    Внес исправления

--- projects/loft-photo/pages.js
+++ projects/loft-photo/pages.js
@@ -10,8 +10,14 @@ export default {
   openPage(name) {
     let select = pagesMap[name];
     let el = document.querySelector(select);
+    let itemElements = document.querySelectorAll('.page');
 
-    currentPage?.classList.add('hidden');
+    itemElements.forEach((item) => {
+      if (!item.classList.contains('hidden')) {
+        item.classList.add('hidden');
+      }
+    });
+
     currentPage = el;
     currentPage.classList.remove('hidden');
   },
